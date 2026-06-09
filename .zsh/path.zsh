ae5f0fefe692d11dd07b8c8a0de3bca7f61ca6ad
sserada <serada.dev@gmail.com>
2026-06-10 01:46:54 +0900

    refactor(zsh): remove pyenv config from path.zsh

--- .zsh/path.zsh
+++ .zsh/path.zsh
@@ -1,7 +1,2 @@
-# Pyenv
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
-
 # pipx (for SuperClaude and other Python tools)
 export PATH="$HOME/.local/bin:$PATH"
